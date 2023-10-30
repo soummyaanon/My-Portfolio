a2e063745ca304410372ad160d5a787f2c34a706
Soumyaranjan panda <91568463+soummyaanon@users.noreply.github.com>
2023-10-30 21:54:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const typed = new Typed('.multiple-text',{
+    strings:['Full stack Web Developer','  Tech Enthusiat','Student'],
+typeSpeed: 100,
+backSpeed: 100,
+backDelay: 100,
+loop: true
+});
