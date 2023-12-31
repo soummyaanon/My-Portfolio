6aa68e15872d09c23c8149ffb55b946a954ffc40
Soumyaranjan panda <91568463+soummyaanon@users.noreply.github.com>
2023-12-31 18:21:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-const typed = new Typed('.multiple-text',{
-    strings:['Full stack Web Developer','  Tech Enthusiat','Student'],
-typeSpeed: 100,
-backSpeed: 100,
-backDelay: 100,
-loop: true
-});
+const typed = new Typed('.multiple-text', {
+    strings: ['S.I.H winner 2023🏆🏆','Full stack Web Developer💻💻',' Tech Enthusiat‍🧑‍💻','Student‍♂️'],
+    typeSpeed: 250,
+    backSpeed: 50,
+    loop: true,
+    backDelay: () => Math.random() * 400 + 200 // Random delay between 200-600ms
+  });
+  
